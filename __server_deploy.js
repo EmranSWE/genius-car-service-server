e2b3d23d7722daa0f1d3c71286760d61590f3b60
EmranSWE <mdemran.swe@gmail.com>
2022-11-25 22:50:26 +0600

    fixed issue

--- __server_deploy.js
+++ __server_deploy.js
@@ -7,4 +7,20 @@ One time
 
 _---------------For every projects
 1.heroku create
+2.git add .,git commit -m "", git push ""
+3.Git push heroku main
+3.Go to heroku dashboard>current project>Reveal config var
+5.Copy paste .env file
+6.Make sure you have whitelisted 
+-------------
+Update server
+*1.Make Change
+2. git add .,git commit -m "", git push ""
+3.Git push heroku main
+---------------
+Connect server with Client
+---------------
+1.replace localhost heroku link
+2.npm run build
+3.firebase deploy
 */
--- index.js
+++ index.js
@@ -13,6 +13,7 @@ app.use(express.json());
 function verifyJWT(req, res, next) {
 
     const authHeader = req.headers.authorization;
+
     if (!authHeader) {
         return res.status(401).send({ message: 'unauthorized user' })
     }
